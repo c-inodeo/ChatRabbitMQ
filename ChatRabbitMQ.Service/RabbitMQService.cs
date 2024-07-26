f5e8a8f728c6c72ca7e30af18b0495330c724a4a
Claire <Claire@LAPTOP-SR6DU3OK>
2024-07-26 17:54:18 +0800

    Add project files.

---
+++ ChatRabbitMQ.Service/RabbitMQService.cs
@@ -0,0 +1,56 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatRabbitMQ.Service
+{
+    public class RabbitMQService
+    {
+        private readonly IConnection _conn;
+        private readonly IModel _channel;
+
+        public RabbitMQService()
+        {
+           var factory = new ConnectionFactory() { HostName = "localhost"};
+            _conn = factory.CreateConnection();
+            _channel = _conn.CreateModel();
+            _channel.QueueDeclare(
+                queue: "message_queue",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+        }
+        public void SendMessage(string message)
+        {
+            var body = Encoding.UTF8.GetBytes(message);
+            _channel.BasicPublish(
+                exchange:"",
+                routingKey:"message_queue",
+                basicProperties: null,
+                body: body
+            );
+        }
+        public void ReceiveMessage(Action<string> handleMessage) 
+        {
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (model, ea) =>
+            { 
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                handleMessage(message);
+            };
+            _channel.BasicConsume(
+                queue: "message_queue",
+                autoAck: true,
+                consumer: consumer
+            );
+        }
+    }
+}
---
+++ ChatRabbitMQ/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChatRabbitMQ.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
